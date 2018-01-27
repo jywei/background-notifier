09463abdd408e07d2c66898527c64414e6df9640
Roy Wei <roywjy@gmail.com>
2018-01-27 14:06:28 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+func main() {
+	c, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	for {
+		fmt.Println(">>Please enter some messages for notification Enter it or \"q\" to exit")
+		bio := bufio.NewReader(os.Stdin)
+		line, _, _ := bio.ReadLine()
+
+		if string(line) == "q" {
+			break
+		}
+
+		terms := strings.Split(string(line), " ")
+		c.Do("SET", "notification", terms)
+		world, err := redis.String(c.Do("GET", "notification"))
+		if err != nil {
+			fmt.Println("key not found")
+		}
+		fmt.Println(world)
+	}
+}

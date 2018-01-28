6be356a976d27caa6f99a4bf0669c0f587f1c52e
Roy Wei <roywjy@gmail.com>
2018-01-28 15:20:12 +0800

    Update on setting

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-`redis-cli -r 100 RPUSH resque:queue:slack '{"class":"notifier","args":["Sen77"]}'`
+`redis-cli -r 100 -p 4000 RPUSH resque:queue:slack '{"class":"notifier","args":["This is a test notification"]}'`
+
+`echo "port 4000" | redis-server -`
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"github.com/benmanns/goworker"
 	"fmt"
 	"time"
+
+	"github.com/benmanns/goworker"
 	"github.com/parnurzeal/gorequest"
 )
 
+// Payload type is for passing JSON format
 type Payload struct {
 	Username  string `json:"username,omitempty"`
 	IconEmoji string `json:"icon_emoji,omitempty"`
@@ -14,11 +16,9 @@ type Payload struct {
 	Text      string `json:"text,omitempty"`
 }
 
-
 func redirectPolicyFunc(req gorequest.Request, via []gorequest.Request) error {
 	return fmt.Errorf("Incorrect token (redirection)")
 }
-
 
 func send(webhookURL string, proxy string, payload Payload) []error {
 	request := gorequest.New().Proxy(proxy)
@@ -37,35 +37,36 @@ func send(webhookURL string, proxy string, payload Payload) []error {
 	return nil
 }
 
-func notificationFunc(payload Payload, webhookURL string, response chan<- []error)  {
+func notificationFunc(payload Payload, webhookURL string, response chan<- []error) {
 	response <- send(webhookURL, "", payload)
 }
 
 func notificationWorker(queue string, args ...interface{}) error {
 	webhookURL := "https://hooks.slack.com/services/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"
 	responseChannel := make(chan []error)
+	message := args[0].(string)
 	payload := Payload{
-		Text:      args[0].(string),
+		Text:      message,
 		Username:  "Hexter Bot",
 		IconEmoji: ":hexter_is_ur_daddy:",
 		Channel:   "#nuclear-testing-sites",
 	}
 	go notificationFunc(payload, webhookURL, responseChannel)
 	fmt.Printf("Send to %s Platform, message content: %v\n", queue, args)
-	err := <- responseChannel
+	err := <-responseChannel
 	return err[0]
 }
 
 func init() {
 	settings := goworker.WorkerSettings{
-		URI: "redis://localhost:6379/",
-		Connections: 100,
-		Queues:	[]string{"slack", "message"},
-		UseNumber: true,
+		URI:            "redis://localhost:4000/",
+		Connections:    3,
+		Queues:         []string{"slack"},
+		UseNumber:      true,
 		ExitOnComplete: false,
-		Concurrency: 2,
-		Namespace: "resque:",
-		Interval: 5.0,
+		Concurrency:    2,
+		Namespace:      "resque:",
+		Interval:       5.0,
 	}
 	goworker.SetSettings(settings)
 	goworker.Register("notifier", notificationWorker)

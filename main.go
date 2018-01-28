535982c320db08f365defd45eda3ab44105cf2b8
Hexter Weng <hexter.ch@gmail.com>
2018-01-28 15:03:57 +0800

    use goworker and combine slack notification function

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	// "bufio"
+	"github.com/benmanns/goworker"
 	"fmt"
-	// "os"
-
-	"github.com/garyburd/redigo/redis"
+	"time"
 	"github.com/parnurzeal/gorequest"
-	"github.com/gin-gonic/gin"
 )
 
-// Payload Type is for saving payload as JSON strcture
 type Payload struct {
 	Username  string `json:"username,omitempty"`
 	IconEmoji string `json:"icon_emoji,omitempty"`
@@ -18,9 +14,11 @@ type Payload struct {
 	Text      string `json:"text,omitempty"`
 }
 
+
 func redirectPolicyFunc(req gorequest.Request, via []gorequest.Request) error {
 	return fmt.Errorf("Incorrect token (redirection)")
 }
+
 
 func send(webhookURL string, proxy string, payload Payload) []error {
 	request := gorequest.New().Proxy(proxy)
@@ -39,69 +37,43 @@ func send(webhookURL string, proxy string, payload Payload) []error {
 	return nil
 }
 
-func notificationWorker(webhookURL string, response chan<- []error) {
-	payload := Payload{
-		Text:      ":thinking_face:",
-		Username:  "Hexter Bot",
-		IconEmoji: ":hexter_is_ur_daddy:",
-		Channel:   "#studygroup-tw",
-	}
+func notificationFunc(payload Payload, webhookURL string, response chan<- []error)  {
 	response <- send(webhookURL, "", payload)
 }
 
-func main() {
-	c, err := redis.Dial("tcp", ":4000")
-	if err != nil {
-		panic(err)
+func notificationWorker(queue string, args ...interface{}) error {
+	webhookURL := "https://hooks.slack.com/services/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"
+	responseChannel := make(chan []error)
+	payload := Payload{
+		Text:      args[0].(string),
+		Username:  "Hexter Bot",
+		IconEmoji: ":hexter_is_ur_daddy:",
+		Channel:   "#nuclear-testing-sites",
 	}
-	defer c.Close()
+	go notificationFunc(payload, webhookURL, responseChannel)
+	fmt.Printf("Send to %s Platform, message content: %v\n", queue, args)
+	err := <- responseChannel
+	return err[0]
+}
 
+func init() {
+	settings := goworker.WorkerSettings{
+		URI: "redis://localhost:6379/",
+		Connections: 100,
+		Queues:	[]string{"slack", "message"},
+		UseNumber: true,
+		ExitOnComplete: false,
+		Concurrency: 2,
+		Namespace: "resque:",
+		Interval: 5.0,
+	}
+	goworker.SetSettings(settings)
+	goworker.Register("notifier", notificationWorker)
+}
 
-	// There code is for console testing
-
-
-	//for {
-	//	fmt.Println(">>Please enter some messages for notification Enter it or \"q\" to exit")
-	//	bio := bufio.NewReader(os.Stdin)
-	//	line, _, _ := bio.ReadLine()
-	//
-	//	if string(line) == "q" {
-	//		break
-	//	}
-	//
-	//	c.Do("SET", "notification", line)
-	//	message, err := redis.String(c.Do("GET", "notification"))
-	//	if err != nil {
-	//		fmt.Println("key not found")
-	//	}
-	//	payload := Payload{
-	//		Text:      message,
-	//		Username:  "Hexter Bot",
-	//		IconEmoji: ":hexter_is_ur_daddy:",
-	//		Channel:   "#studygroup-tw",
-	//	}
-	//	sendErr := send(webhookURL, "", payload)
-	//	if sendErr != nil {
-	//		fmt.Printf("error: %s\n", err)
-	//	}
-	//}
-
-	r := gin.Default()
-	r.GET("/notification", func(c *gin.Context) {
-		webhookURL := "https://hooks.slack.com/services/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"
-		responseChannel := make(chan []error)
-		go notificationWorker(webhookURL, responseChannel)
-		err := <- responseChannel
-		if err != nil {
-			c.JSON(400, gin.H{
-				"message": err,
-			})
-		} else {
-			c.JSON(200, gin.H{
-				"message": "Goodjob",
-			})
-		}
-		close(responseChannel)
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+func main() {
+	if err := goworker.Work(); err != nil {
+		fmt.Println("Error", err)
+	}
+	fmt.Printf("Started on %v", time.Now().Format("2006-01-02 15:04:05"))
 }
